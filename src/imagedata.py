ed3c16919c87ae167d61899a21cf745cc7f5475b
wangleihitcs <wanglei101100@gmail.com>
2017-12-07 11:26:20 +0800

    2

--- src/imagedata.py
+++ src/imagedata.py
@@ -1,1 +1,9 @@
 import numpy as np
+import tensorflow as tf
+a = tf.random_uniform([5, 3], minval=0.0, maxval=10.0, dtype=tf.float32, name='a')
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter('./graphs', sess.graph)
+    print(sess.run(a))
+
+writer.close()
