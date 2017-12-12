4ba3e08f98e0eb424dab5c11e83d13ff85f13592
wangleihitcs <wanglei101100@gmail.com>
2017-12-12 17:41:25 +0800

    the first success, modification!

--- src/FaceEnhance.py
+++ src/FaceEnhance.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 # 创建变量，存储在CPU中
 def variable_on_cpu(name, shape, initializer, dtype):
-    # with tf.device('/cpu:0'):
-    var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
     # var = tf.Variable(tf.random_normal(shape=shape, mean=0, stddev=0.1))
     return var
 
@@ -24,11 +24,13 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 # 池化
 def max_pool_2x2(x, name):
-    return tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
 # 处理获取数据集
-def inputs():
-    images = FaceInput.get_trains()
-    labels = FaceInput.get_labels()
+def inputs(start, end):
+    images = FaceInput.get_trains(start, end)
+    labels = FaceInput.get_labels(start, end)
+    # images = tf.cast(images, tf.float32)
+    # labels = tf.cast(labels, tf.float32)
     return images, labels
 
 # 神经网络模型
@@ -176,32 +178,49 @@ def compute_loss(output_images, label_images):
     loss = tf.reduce_mean(tf.square(label_images - output_images))
     return loss
 
-def train(input_images, label_images, image_sizes, iters, save_path):
+def train(iters, batch_size, train_num, model_path):
     xs = tf.placeholder(tf.float32, [None, 256, 256, 3])
     ys = tf.placeholder(tf.float32, [None, 256, 256, 3])
-    
-    output_images = neural_networks_model(xs, 16)
+
+    output_images = neural_networks_model(xs, batch_size)
     cost_function = compute_loss(output_images, ys)
     train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(cost_function)
 
     saver = tf.train.Saver()
-
+    file_log = open('log.txt', 'wt')
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
+        saver.restore(sess, model_path)
+        # writer = tf.summary.FileWriter('./graphs', sess.graph)
         for i in range(iters):
-            for t in range(0, image_sizes-16, 16):
-                xs_batch, ys_batch = sess.run([input_images[t:t+16], label_images[t:t+16]])
-                sess.run(train_step, feed_dict={xs: xs_batch, ys: ys_batch})
-                if t % 1 == 0:
+            for t in range(0, train_num-batch_size, batch_size):
+                xs_batch, ys_batch = inputs(t, t+batch_size)
+                sess.run(train_step, feed_dict={xs:xs_batch, ys:ys_batch})
+                if t % 10 == 0:
                     cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
-                    print('iters:%s, loss:%s' % (t, cost))
-    saver.save(sess, save_path)
+                    print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
+                    file_log.write('iters:%s, batch_add:%s, loss:%s \n' % (i, t, cost))
+            if i % 100 == 0:
+                saver.save(sess, model_path)
+    
+    # writer.close()
     sess.close()
 
-X, Y = inputs()
-X = tf.cast(X, tf.float32)
-Y = tf.cast(Y, tf.float32)
-print(X.shape)
-# neural_networks_model(X[:32], 32)
-save_path = '/home/wanglei/wl/face-enhance/model/model1.ckpt'
-train(X, Y, 500, 10000, save_path)
+def predict(input_image, save_path):
+    output_image = neural_networks_model(input_image, 1)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, save_path)
+        predict_image = output_image.eval(session = sess)
+        print(predict_image)
+        # fig = plt.figure()
+        # plt.imshow(predict_image)
+        # plt.show()
+
+iters = 10000
+batch_size = 32
+train_num = 5000
+model_path = '/home/wanglei/wl/face-enhance/model/model1.ckpt'
+train(iters, batch_size, train_num, model_path)
+# input_image = tf.reshape(X[0], [1, 256, 256, 3])
+# predict(input_image, save_path)
