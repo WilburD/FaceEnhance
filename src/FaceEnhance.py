ffca0ff3780f1df63e2e8a031761d30c046b5f2b
wangleihitcs <wanglei101100@gmail.com>
2017-12-15 22:19:17 +0800

    test to to

--- src/FaceEnhance.py
+++ src/FaceEnhance.py
@@ -33,28 +33,28 @@ def train(iters, batch_size, train_num, model_path, image_size, model_type):
 
     output_images = neural_networks_model(xs, batch_size, image_size, model_type)
     cost_function = compute_loss(output_images, ys)
-    train_step = tf.train.GradientDescentOptimizer(0.12).minimize(cost_function)
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cost_function)
     # train_step = tf.train.AdamOptimizer(1e-4).minimize(cost_function)
 
     saver = tf.train.Saver()
     file_log = open('log.txt', 'wt')
     # loss = []
     with tf.Session() as sess:
-        # sess.run(tf.global_variables_initializer())
-        saver.restore(sess, model_path)
+        sess.run(tf.global_variables_initializer())
+        # saver.restore(sess, model_path)
         # writer = tf.summary.FileWriter('./graphs', sess.graph)
         for i in range(iters):
             for t in range(0, train_num-batch_size, batch_size):
                 xs_batch, ys_batch = inputs(t, t+batch_size, image_size)
                 sess.run(train_step, feed_dict={xs:xs_batch, ys:ys_batch})
-                if t % 32 == 0:
+                if t % 4 == 0:
                     cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
                     # loss.append(cost)
                     print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
                     file_log.write('iters:%s, batch:%s, loss:%s \n' % (i, t, cost))
             file_log.write('--------------------------------------------------------------\n')
             print('--------------------------------------------------------------')
-            if i % 20 == 0:
+            if i % 2 == 0:
                 saver.save(sess, model_path)
                 print('*******************保存成功*******************')
     
@@ -86,25 +86,25 @@ def u_net_main():
     iters = 1000 # 迭代次数
     batch_size = 32
     train_num = 256 # 训练集数量
-    image_size = 64
+    image_size = 256
     model_type = 'unet'
     model_path_unet = '/home/wanglei/wl/model/model_unet.ckpt' # UNet model 256x256
     model_path_unet_64x64 = '/home/wanglei/wl/model/model_unet_64x64.ckpt' # UNet model 64x64
 
-    t = 100001
-    imagedata = FaceInput.ImageData(0, 1, 256)
-    x = imagedata.get_image_by_path('/home/wanglei/wl/face-enhance/resource/yangmi22.jpg')
-    y = imagedata.get_image_by_path('/home/wanglei/wl/face-enhance/resource/yangmi11.jpg')
+    # t = 100001
+    # imagedata = FaceInput.ImageData(0, 1, 256)
+    # x = imagedata.get_image_by_path('/home/wanglei/wl/face-enhance/resource/yangmi22.jpg')
+    # y = imagedata.get_image_by_path('/home/wanglei/wl/face-enhance/resource/yangmi11.jpg')
 
-    # t = 0
-    # x, y = inputs(t, t+1, image_size)
+    t = 302
+    x, y = inputs(t, t+1, image_size)
 
     if image_size == 256:
-        # train(iters, batch_size, train_num, model_path_unet, image_size, model_type)
-        predict(x, y, model_path_unet_64x64, image_size, model_type, t)
-    else : # 64x64 
-        train(iters, batch_size, train_num, model_path_unet_64x64, image_size, model_type)
+        train(iters, batch_size, train_num, model_path_unet, image_size, model_type)
         # predict(x, y, model_path_unet_64x64, image_size, model_type, t)
+    else : # 64x64 
+        # train(iters, batch_size, train_num, model_path_unet_64x64, image_size, model_type)
+        predict(x, y, model_path_unet_64x64, image_size, model_type, t)
     
 
 # 训练测试EncodeDecode model
