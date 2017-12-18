c8a179449f417c716629ec8aa1dda4375c92845f
wangleihitcs <wanglei101100@gmail.com>
2017-12-18 22:10:56 +0800

    error

--- README.md
+++ README.md
@@ -5,15 +5,15 @@
 本系统目的是使模糊的人脸图片变清晰。
 
 ## 网络结构
-#### UNetLike
-![image](https://github.com/wangleihitcs/face-enhance/raw/master/resource/UNetLike.png)
+#### 第一阶段
+
+#### 第二阶段
 
 ## 测试
 #### 测试1
-![image](https://github.com/wangleihitcs/face-enhance/raw/master/resource/test1.png)
-
-#### 测试2
+loss = 0.00070
 ![image](https://github.com/wangleihitcs/face-enhance/raw/master/resource/test2.png)
 
-#### 测试3
+#### 测试2
+loss = 0.00028
 ![image](https://github.com/wangleihitcs/face-enhance/raw/master/resource/test3.png)
--- src/FaceEnhance.py
+++ src/FaceEnhance.py
@@ -32,37 +32,36 @@ def train(iters, batch_size, train_num, model_path, image_size, model_type):
     ys = tf.placeholder(tf.float32, [None, image_size, image_size, 3])
 
     output_images = neural_networks_model(xs, batch_size, image_size, model_type)
-    cost_function = compute_loss(output_images, ys)
-    train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cost_function)
+    cost_function = compute_loss(output_images[:, 100:156, 100:128], ys[:, 100:156, 100:128])
+    train_step = tf.train.GradientDescentOptimizer(0.012).minimize(cost_function)
     # train_step = tf.train.AdamOptimizer(1e-4).minimize(cost_function)
 
     saver = tf.train.Saver()
     file_log = open('log.txt', 'wt')
-    # loss = []
     with tf.Session() as sess:
-        # sess.run(tf.global_variables_initializer())
-        saver.restore(sess, model_path)
+        sess.run(tf.global_variables_initializer())
+        # saver.restore(sess, model_path)
         # writer = tf.summary.FileWriter('./graphs', sess.graph)
 
         for i in range(iters):
-            
+            print('--------------------------------------------------------------')
+            file_log.write('--------------------------------------------------------------\n')
             for t in range(0, train_num-batch_size, batch_size):
                 xs_batch, ys_batch = inputs(t, t+batch_size, image_size)
                 sess.run(train_step, feed_dict={xs:xs_batch, ys:ys_batch})
-                # if t % 1 == 0:
-                #     cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
-                #     # loss.append(cost)
-                #     print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
-                #     file_log.write('iters:%s, batch:%s, loss:%s \n' % (i, t, cost))
-            if i % 50 == 0:
-                print('--------------------------------------------------------------')
-                t = 0
-                cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
-                # loss.append(cost)
-                print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
-                file_log.write('iters:%s, batch:%s, loss:%s \n' % (i, t, cost))
+                if t % 1 == 0:
+                    cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
+                    print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
+                    file_log.write('iters:%s, batch:%s, loss:%s \n' % (i, t, cost))
+            # if i % 50 == 0:
+            #     print('--------------------------------------------------------------')
+            #     t = 0
+            #     cost = sess.run(cost_function, feed_dict={xs: xs_batch, ys: ys_batch})
+            #     # loss.append(cost)
+            #     print('iters:%s, batch_add:%s, loss:%s' % (i, t, cost))
+            #     file_log.write('iters:%s, batch:%s, loss:%s \n' % (i, t, cost))
             # file_log.write('--------------------------------------------------------------\n')
-            if i % 500 == 0:
+            if i % 20 == 0:
                 saver.save(sess, model_path)
                 print('***********************保存成功***********************')
     
@@ -86,7 +85,7 @@ def predict(input_image, label_image, save_path, image_size, model_type, it):
         ax = fig.add_subplot(1, 3, 3)
         ax.imshow(label_image[0])
         plt.axis('off')
-        # plt.savefig('/home/wanglei/图片/' + str(it) + '.png')
+        plt.savefig('/home/wanglei/图片/' + str(it) + '.png')
         plt.show()
 
 # 训练测试UNet model
@@ -110,8 +109,8 @@ def u_net_main():
     x, y = inputs(t, t+1, image_size)
 
     if image_size == 256:
-        # train(iters, batch_size, train_num, model_path_unet, image_size, model_type)
-        predict(x, y, model_path_unet, image_size, model_type, t)
+        train(iters, batch_size, train_num, model_path_unet, image_size, model_type)
+        # predict(x, y, model_path_unet, image_size, model_type, t)
         # predict(x, y, model_path_unet_64x64, image_size, model_type, t)
     else : # 64x64 
         # train(iters, batch_size, train_num, model_path_unet_64x64, image_size, model_type)
